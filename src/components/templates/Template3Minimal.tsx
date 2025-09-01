29fdc1695b1f59bfd984650a04bd67a621f81564
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-01 13:14:13 +0100

    Updated Template3Minimal.tsx

--- src/components/templates/Template3Minimal.tsx
+++ src/components/templates/Template3Minimal.tsx
@@ -179,13 +179,8 @@ export default function Template3Minimal({ data, type }: TemplateProps) {
 
       {/* FOOTER */}
       <div className="bg-[#0a1f44] text-white p-6 text-center text-sm relative z-10">
-        <p>
+      <p>
          <strong> {user?.company.name} </strong>  | {user?.company.address} | <strong>Tél :</strong> {user?.company.phone} | <strong>ICE :</strong> {user?.company.ice} |  <strong> IF:</strong> {user?.company.if} | <strong> RC:</strong> {user?.company.rc} | <strong> CNSS:</strong> {user?.company.cnss} | <strong> Patente :</strong> {user?.company.patente} | <strong> EMAIL :</strong> {user?.company.email} | <strong> SITE WEB  :</strong> {user?.company.website}
-          <strong>Tél :</strong> {user?.company.phone} | <strong>ICE :</strong>{' '}
-          {user?.company.ice} | <strong> IF:</strong> {user?.company.if} |{' '}
-          <strong> RC:</strong> {user?.company.rc} | <strong> CNSS:</strong>{' '}
-          {user?.company.cnss} | <strong> Patente :</strong> {user?.company.rc}{' '}
-          | <strong> EMAIL :</strong> | <strong> SITE WEB :</strong>
         </p>
       </div>
     </div>
