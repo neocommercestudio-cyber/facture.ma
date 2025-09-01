10fde6e8e9a92202ba07cc94908dc2a67cb5432d
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-01 16:49:08 +0100

    Updated InvoiceViewer.tsx

--- src/components/invoices/InvoiceViewer.tsx
+++ src/components/invoices/InvoiceViewer.tsx
@@ -445,18 +445,14 @@ const generateTemplate2HTML = () => {
       </div>
 
       <!-- FOOTER -->
-      <div style="background:black; color:white; border-top:2px solid white; padding:20px; text-align:center; font-size:14px;">
+      <div style="padding:32px 20px 20px;position:relative;z-index:10;">
         <p>
-        <strong>${user?.company.name || ""}</strong> | ${user?.company.address || ""} |
-        <strong>Tél :</strong> ${user?.company.phone || ""} |
-        <strong>ICE :</strong> ${user?.company.ice || ""} |
-        <strong>IF :</strong> ${user?.company.if || ""} |
-        <strong>RC :</strong> ${user?.company.rc || ""} |
-        <strong>CNSS :</strong> ${user?.company.cnss || ""} |
-        <strong>Patente :</strong> ${user?.company.rc || ""} |
-        <strong>EMAIL :</strong> ${user?.company.email || ""} |
-        <strong>SITE WEB :</strong> ${user?.company.website || ""}
-      </p>
+          <strong>${user?.company.name}</strong> | ${user?.company.address} | 
+          <strong>Tél :</strong> ${user?.company.phone} | <strong>ICE :</strong> ${user?.company.ice} |
+          <strong>IF :</strong> ${user?.company.if} | <strong>RC :</strong> ${user?.company.rc} |
+          <strong>CNSS :</strong> ${user?.company.cnss} | <strong>Patente :</strong> ${user?.company.rc} |
+          <strong>EMAIL :</strong> ${user?.company.email} | <strong>SITE WEB :</strong> ${user?.company.website}
+        </p>
       </div>
 
     </div>
