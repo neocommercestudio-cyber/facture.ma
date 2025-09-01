2aa202f13e5c0118bebc587942058e4e385dd3f3
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-01 17:03:14 +0100

    Updated InvoiceViewer.tsx

--- src/components/invoices/InvoiceViewer.tsx
+++ src/components/invoices/InvoiceViewer.tsx
@@ -914,6 +914,11 @@ const generateTemplate4HTML = () => {
   `;
 };
 
+
+
+
+
+  
   return (
     <div className="fixed inset-0 z-50 overflow-y-auto bg-gray-500 bg-opacity-75">
       <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
