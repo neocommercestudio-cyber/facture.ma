373016ea3ce4661cad8acea69e5d3f581b49b71a
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-03 00:57:36 +0100

    Updated Template5Premium.tsx

--- src/components/templates/Template5Premium.tsx
+++ src/components/templates/Template5Premium.tsx
@@ -102,7 +102,7 @@ export default function Template5Premium({ data, type }: TemplateProps) {
           <div className="text-sm font-bold pt-3 text-center  pb-4">
             <p>Arrêtée le présent {type === 'invoice' ? 'facture' : 'devis'} à la somme de :</p>
             
-            <p className="text-sm font-bold border-t border-[#03224C] pt-2 text-[#0a1f44]">• {data.totalInWords}</p>
+            <p className="text-sm font-bold border-t border-[#03224C] pt-2 text-[#0a1f44] pb-4">• {data.totalInWords}</p>
           </div>
  </div>
           {/* Bloc droit */}
