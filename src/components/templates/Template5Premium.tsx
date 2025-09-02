44d00d83c05ed869b0f2f34a2e2b2b0dddd47b0c
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-03 00:55:08 +0100

    Updated Template5Premium.tsx

--- src/components/templates/Template5Premium.tsx
+++ src/components/templates/Template5Premium.tsx
@@ -98,8 +98,8 @@ export default function Template5Premium({ data, type }: TemplateProps) {
         {/* TOTALS */}
         <div className="p-8 flex justify-between">
           {/* Bloc gauche */}
-           <div className="w-80 bg-gray-50 rounded border border-[#24445C] p-4">
-          <div className="text-sm font-bold pt-3 text-center text-[#24445C] pb-4">
+           <div className="w-80 bg-gray-50 rounded border border-[#0a1f44] p-4">
+          <div className="text-sm font-bold pt-3 text-center text-[#03224C] pb-4">
             <p>Arrêtée le présent {type === 'invoice' ? 'facture' : 'devis'} à la somme de :</p>
             <p className="text-sm font-bold border-t border-[#03224C] pt-2 text-[#03224C]">• {data.totalInWords}</p>
           </div>
