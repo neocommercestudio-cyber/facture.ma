6ef476a7c906c2913d1573f11c9c8657da380e25
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-02 23:47:25 +0100

    Updated Template2Modern.tsx

--- src/components/templates/Template2Modern.tsx
+++ src/components/templates/Template2Modern.tsx
@@ -88,7 +88,7 @@ export default function Template2Modern({ data, type }: TemplateProps) {
         <div className="flex justify-between">
           {/* Bloc gauche */}
           <div className="w-80 bg-gray-50 border border-black rounded p-2">
-            <div className="text-lg font-bold border-black pt-3 text-center">
+            <div className="text-lg font-bold border-black pt-3 text-center pb-4">
               <p>Arrêtée le présent {type === 'invoice' ? 'facture' : 'devis'} à la somme de :</p>
             </div>
             <div className="text-lg border-t border-black pt-3">
