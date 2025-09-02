0386df4d9456c8028ea8330d4169d82d6302d66d
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-02 17:59:25 +0100

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -43,12 +43,7 @@ export default function Header({ sidebarOpen, setSidebarOpen }: HeaderProps) {
           </div>
 
           {/* Notifications */}
-          <button className="relative p-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-lg transition-colors">
-            <Bell className="w-5 h-5" />
-            <span className="absolute -top-1 -right-1 w-4 h-4 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">
-              3
-            </span>
-          </button>
+        
 
           {/* Company Name */}
           {user?.company?.name && (
