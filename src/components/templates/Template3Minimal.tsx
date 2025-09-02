ec79ce7ea8b47513dccd29a8d429a8779db4aa7c
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-02 23:55:15 +0100

    Updated Template3Minimal.tsx

--- src/components/templates/Template3Minimal.tsx
+++ src/components/templates/Template3Minimal.tsx
@@ -36,7 +36,7 @@ export default function Template3Minimal({ data, type }: TemplateProps) {
           <img
             src={user.company.logo}
             alt="Logo"
-            className="h-20 w-20 mx-auto mb-4"
+            className="h-20 w-30 mx-auto mb-4"
           />
         )}
         <h1 className="text-5xl font-extrabold text-[#0a1f44]">
