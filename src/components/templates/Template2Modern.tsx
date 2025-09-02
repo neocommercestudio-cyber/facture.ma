279d771d4aba95a12407c5938fa9c6facd4e8bd4
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-03 00:18:58 +0100

    Updated Template2Modern.tsx

--- src/components/templates/Template2Modern.tsx
+++ src/components/templates/Template2Modern.tsx
@@ -162,7 +162,7 @@ export default function Template2Modern({ data, type }: TemplateProps) {
 </div>
 
       {/* FOOTER */}
-      <div className="bg-black text-white border-t-2 border-white p-6 text-base text-center">
+      <div className="bg-black text-white border-t-2 border-white p-6 text-xs text-center">
       <p>
          <strong> {user?.company.name} </strong>  | {user?.company.address} | <strong>Tél :</strong> {user?.company.phone} | <strong>ICE :</strong> {user?.company.ice} |  <strong> IF:</strong> {user?.company.if} | <strong> RC:</strong> {user?.company.rc} | <strong> CNSS:</strong> {user?.company.cnss} | <strong> Patente :</strong> {user?.company.patente} | <strong> EMAIL :</strong> {user?.company.email} | <strong> SITE WEB  :</strong> {user?.company.website}
         </p>
