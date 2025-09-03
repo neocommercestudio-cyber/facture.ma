96ce716118859d77175c9e44d9144870509f6ad6
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-03 01:14:52 +0100

    Updated Template5Premium.tsx

--- src/components/templates/Template5Premium.tsx
+++ src/components/templates/Template5Premium.tsx
@@ -37,7 +37,7 @@ export default function Template5Premium({ data, type }: TemplateProps) {
 
         {/* vague rouge */}
         <svg
-          className="absolute top-5 left-0 w-full"
+          className="absolute top--5 left-0 w-full"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 80"
         >
