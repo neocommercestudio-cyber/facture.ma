079a6eb1ecde21baf0c5a8cad7f4694229d23833
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-03 01:03:17 +0100

    Updated Template5Premium.tsx

--- src/components/templates/Template5Premium.tsx
+++ src/components/templates/Template5Premium.tsx
@@ -98,8 +98,9 @@ export default function Template5Premium({ data, type }: TemplateProps) {
         {/* TOTALS */}
         <div className="p-8 flex justify-between">
           {/* Bloc gauche */}
-          <div className="w-80 bg-gray-50 rounded border border-[#0a1f44] p-4 text-center">
+          <div className="w-80 text-sm bg-gray-50 rounded border border-[#0a1f44] p-4 text-center">
             <p>Arrêtée le présent {type === 'invoice' ? 'facture' : 'devis'} à la somme de :</p>
+            
             <p className="text-sm font-bold border-t border-[#03224C] pt-2 text-[#03224C]">• {data.totalInWords}</p>
           </div>
 
